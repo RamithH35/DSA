85da76b6d5b753bfcd3596cdf2e34b71bcb27551
RamithH35 <ramithh18@gmail.com>
2026-08-04 18:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (80.65%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        vector<int> v;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (find(v.begin(), v.end(), nums[i]) != v.end())
-                v.erase(remove(v.begin(), v.end(), nums[i]), v.end());
-            else
-                v.push_back(nums[i]);
+        long n=0;
+        for(int i:nums)
+            n=n^i;
+        long m=(n&(n-1));
+        n=n^m;
+        int one=0;
+        int zero=0;
+        for(int x:nums)
+        {
+            if(x&n)
+                one=one^x;
+            else 
+                zero=zero^x;
         }
-
-        return v;
+        return{one,zero};
     }
 };
