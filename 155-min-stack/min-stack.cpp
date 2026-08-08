c634e8706866f9fb65aca37694310fab0b1845e3
RamithH35 <ramithh18@gmail.com>
2026-08-09 00:30:38 +0530

    Time: 61 ms (48.22%) | Memory: 151 MB (78.78%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,30 +1,32 @@
 class MinStack {
 public:
-    stack<pair<int,int>>st;
-    MinStack() {
+    stack<pair<int,int>> s;
+    MinStack() {}
+    
+    void push(int value) {
+        
+        if(!s.empty()){
+            s.push({value,min(value,s.top().second)});
+        }
+        else{
+            s.push({value,value});
+        }
         
     }
     
-    void push(int value) {
-        if(st.empty())
-            st.push({value,value});
-        else
-        {
-            int x = min(value,st.top().second);
-            st.push({value,x});
-        }
-    }
-    
     void pop() {
-        st.pop();
+        if(!s.empty()){
+            s.pop();
+        }
+           
     }
     
     int top() {
-        return st.top().first;
+        return s.top().first;  
     }
     
     int getMin() {
-        return st.top().second;
+        return s.top().second;    
     }
 };
 
