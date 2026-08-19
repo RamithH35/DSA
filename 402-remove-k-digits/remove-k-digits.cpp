5dd40c821c98edd5422bfc75618219d562a2db77
RamithH35 <ramithh18@gmail.com>
2026-08-19 22:30:31 +0530

    Time: 2 ms (70.32%) | Memory: 11.1 MB (91.09%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
         string st;
-        for(char c:num)
-        {
-            while(!st.empty()&&k>0&&st.back()>c)
-            {
+        for (char c : num) {
+            while (!st.empty() && k > 0 && st.back() > c) {
                 st.pop_back();
                 k--;
             }
+
             st.push_back(c);
         }
-        while(k>0)
-        {
+        while (k > 0) {
             st.pop_back();
             k--;
         }
-        int pos=st.find_first_not_of('0');
-        if(pos==string::npos)
+        int pos = st.find_first_not_of('0');
+        if (pos == string::npos)
             return "0";
+
         return st.substr(pos);
     }
 };
