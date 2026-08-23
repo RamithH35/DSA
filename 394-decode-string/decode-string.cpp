6eb62e6840013e9f26cc8e16fb6a53b9b6ce591b
RamithH35 <ramithh18@gmail.com>
2026-08-23 23:55:57 +0530

    Time: 2 ms (23.84%) | Memory: 8.6 MB (91.89%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     string decodeString(string s) {
-        string curstr="";
-        int n=0;
-        stack<int>s1;
-        stack<string>s2;
+        stack<char>st;
         for(char c:s)
         {
-            if(isdigit(c))
-                n=(n*10)+(c-'0');
-            else if(c =='[')
-            {
-                s1.push(n);
-                s2.push(curstr);
-                n=0;
-                curstr="";
-            }
-            else if(c==']')
-            {
-                int m=s1.top();
-                s1.pop();
-                string temp=curstr;
-                for(int i=1;i<m;i++)
-                    curstr+=temp;
-                string prev=s2.top();
-                s2.pop();
-                curstr=prev+curstr;
-            }
+            if(c!=']')
+                st.push(c);
             else
-                curstr+=c;
+            {
+                string curstr="";
+                while(!st.empty()&&st.top()!='[')
+                {
+                    curstr+=st.top();
+                    st.pop();
+                }
+                reverse(curstr.begin(),curstr.end());
+                st.pop();
+                string n="";
+                while(!st.empty()&&isdigit(st.top()))
+                {
+                    n+=st.top();
+                    st.pop();
+                }
+                reverse(n.begin(),n.end());
+                int num=stoi(n);
+                string temp=curstr;
+                for(int i=1;i<num;i++)
+                    curstr+=temp;
+                for(char c:curstr)
+                    st.push(c);
+            }
         }
-        return curstr;
+        string ans="";
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
