5cdb129ba2df8734facaffe056bb9465f23007d0
RamithH35 <ramithh18@gmail.com>
2026-08-25 22:30:13 +0530

    Time: 33 ms (51.37%) | Memory: 97.2 MB (5.07%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,18 +1,20 @@
 class StockSpanner {
 public:
-    stack<pair<int,int>>st;
-    int ind=-1;
+    stack<pair<int,int>> st;
+    int indx = -1;
+
     StockSpanner() {
-        ind=-1;
+
     }
     
     int next(int price) {
-        ind++;
-        while(!st.empty()&&st.top().first<=price)
-            st.pop();
-        int left=st.empty()?-1:st.top().second;
-        st.push({price,ind});
-        return ind-left;
+        indx++;
+        while(!st.empty() && st.top().first <= price) st.pop();
+
+        int ans = indx - (st.empty()? -1 : st.top().second);
+        st.push({price,indx});
+
+        return ans;
     }
 };
 
