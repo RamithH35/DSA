95153b8b7a402a3a1d7285a057e301f49d09138c
RamithH35 <ramithh18@gmail.com>
2026-08-28 16:18:57 +0530

    Time: 4 ms (71.69%) | Memory: 48.2 MB (88.79%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        stack<int>st;
+        int ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(st.empty() || nums[i]<nums[st.top()])
+                st.push(i);
+        }
+        for(int j=nums.size()-1;j>=0;j--)
+        {
+            while(!st.empty() && nums[j]>=nums[st.top()])
+            {
+                ans=max(ans,j-st.top());
+                st.pop();
+            }
+        }
+        return ans;
+
+        
+    }
+};
