4e72059c799628df0266e0bf9f63345d65438c39
RamithH35 <ramithh18@gmail.com>
2026-08-30 14:34:46 +0530

    Time: 8 ms (83.85%) | Memory: 70.7 MB (47.89%) - LeetSync

---
+++ 456-132-pattern/132-pattern.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int two=INT_MIN;
+        stack<int>st;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            if(nums[i]<two)
+                return true;
+            while(!st.empty()&&nums[i]>st.top())
+            {
+                two=st.top();
+                st.pop();
+            }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+};
