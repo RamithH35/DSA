0847bb6b89f6777ae79760f6ff652ddc28cfe25b
RamithH35 <ramithh18@gmail.com>
2026-09-02 18:08:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (89.84%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-    void generate(string s,int ind,int n,string d,vector<string>&fn,vector<string>&res)
-    {
-        if(ind==n)
-        {
+    void generate(string& s, int ind, string& digits, vector<string>& fn,
+                  vector<string>& res) {
+
+        if (ind == digits.size()) {
             res.push_back(s);
             return;
         }
-        int x = d[ind]-'0';
-        string y=fn[x];
-        for(int i=0;i<fn[x].size();i++)
-        {
-            generate(s+y[i],ind+1,n,d,fn,res);
+
+        int x = digits[ind] - '0';
+
+        for (int i = 0; i < fn[x].size(); i++) {
+            s.push_back(fn[x][i]);
+
+            generate(s, ind + 1, digits, fn, res);
+
+            s.pop_back();
         }
     }
+
     vector<string> letterCombinations(string digits) {
-        vector<string>fn={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        vector<string>res;
-        string s="";
-        generate(s,0,digits.size(),digits,fn,res);
+        if (digits.empty())
+            return {};
+
+        vector<string> fn = {"",    "",    "abc",  "def", "ghi",
+                             "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        vector<string> res;
+        string s;
+
+        generate(s, 0, digits, fn, res);
+
         return res;
     }
 };
