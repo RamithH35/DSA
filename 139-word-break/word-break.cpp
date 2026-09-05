a0d2af683f4be439c7d366b9f7db846d3f6d7987
RamithH35 <ramithh18@gmail.com>
2026-09-05 22:09:40 +0530

    Time: 2 ms (74.35%) | Memory: 12.2 MB (66.90%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    bool helper(int index,string &s,unordered_set<string>&mp,vector<int>&dp)
-    {
-        if(index==s.size())
+    bool solve(int idx, string &s, unordered_set<string> &st,
+               vector<int> &dp) {
+        
+        if (idx == s.size())
             return true;
-        if(dp[index]!=-1)
-            return dp[index];
-        for(int i=index;i<s.size();i++)
-        {
-            string st = s.substr(index,i-index+1);
-            if(mp.find(st)!=mp.end())
-            {
-                if(helper(i+1,s,mp,dp))
-                {
-                    dp[index]=1;
-                    return true;
-                }
+
+        if (dp[idx] != -1)
+            return dp[idx];
+
+        string temp = "";
+
+        for (int i = idx; i < s.size(); i++) {
+            temp += s[i];
+
+            if (st.find(temp) != st.end()) {
+                if (solve(i + 1, s, st, dp))
+                    return dp[idx] = 1;
             }
         }
-        dp[index]=0;
-        return false;
+
+        return dp[idx] = 0;
     }
+
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string>mp(wordDict.begin(),wordDict.end());
-        vector<int>dp(s.size(),-1);
-        return helper(0,s,mp,dp);
+        unordered_set<string> st(wordDict.begin(), wordDict.end());
+
+        vector<int> dp(s.size(), -1);
+
+        return solve(0, s, st, dp);
     }
 };
