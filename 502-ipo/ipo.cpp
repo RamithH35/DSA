19f54ba1566133b1eef6efa6c276a2ae1eb25b00
RamithH35 <ramithh18@gmail.com>
2026-09-19 21:44:55 +0530

    Time: 198 ms (8.93%) | Memory: 176 MB (5.50%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
-    struct cmp{
-        bool operator()(const pair<int,int>&a,const pair<int,int>&b)
-        {
-            if(a.second!=b.second)
-                return b.second>a.second;
-            return b.first<a.first;
-        }
-    };
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         vector<vector<int>>res;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>pq;
+        priority_queue<int>pq;
         int i=0;
         int n=profits.size();
         while(i<n)
@@ -23,14 +15,14 @@ public:
         while(k>0)
         {
             while(i<n && res[i][0]<=w){
-                pq.push({res[i][0],res[i][1]});
+                pq.push(res[i][1]);
                 i++;
             }
             if(pq.empty())
                 return w;
             auto c=pq.top();
             pq.pop();
-            w+=c.second;
+            w+=c;
             k--;
         }
         return w;
